5bfcf848305ce4f7ffc22acfd539eef5939d74fc
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-12-20 21:03:32 +0100

    feat: the board added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  renderingThesquares() {
+    const arr = [];
+    for (let i = 0; i < 8; i++) {
+      arr.push(...Array(8).fill(i));
+    }
+
+    return arr.map((el, i) => {
+      return (
+        <div
+          className="Square"
+          key={i}
+          id={
+            el % 2
+              ? i % 2 ? "DarkSquare" : "LightSquare"
+              : i % 2 ? "LightSquare" : "DarkSquare"
+          }
+        />
+      );
+    });
+  }
+
+  addingARook() {
+    window.getElementById();
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="MaxWidth">
+        <div className="BoardPositions">
+          <div className="TheBoard">{this.renderingThesquares()}</div>
+        </div>
+      </div>
+    );
   }
 }
 
